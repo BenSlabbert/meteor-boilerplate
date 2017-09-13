be9c25731392477d0820725ecf7fab85e56eead7
Ben Slabbert <ben@hex.co.za>
2017-09-13 14:08:25 +0200

    adding tests for notes

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -19,81 +19,116 @@ if (Meteor.isServer) {
             Notes.insert(note1);
         });
 
-        // afterEach also available
+        describe('insert', function () {
+            it('should insert new note', function () {
+                const userId = 'some user id';
+                const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId });
 
-        it('should insert new note', function () {
-            const userId = 'some user id';
-            const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId });
-
-            expect(Notes.findOne({ _id, userId })).toExist();
-        });
-
-        it('not insert note if not authenticated', function () {
-
-            expect(() => {
-                Meteor.server.method_handlers['notes.insert']();
-            }).toThrow();
-        });
-
-        it('should remove note', function () {
-            Meteor.server.method_handlers['notes.remove'].apply({ userId: note1.userId }, [note1._id]);
-
-            expect(Notes.findOne({ _id: note1._id })).toNotExist();
-        });
-
-        it('should not remove not, not authenticated', function () {
-
-            expect(() => {
-                Meteor.server.method_handlers['notes.remove'].apply({}, [note1._id]);
-            }).toThrow();
-        });
-
-        it('should not remove not, no note ID', function () {
-
-            expect(() => {
-                Meteor.server.method_handlers['notes.remove'].apply({ userId: note1.userId }, ['']);
-            }).toThrow();
-        });
-
-        it('should update note', function () {
-            const title = 'updated title';
-            Meteor.server.method_handlers['notes.update'].apply({
-                userId: note1.userId
-            }, [
-                    note1._id,
-                    { title }
-                ]
-            );
-
-            const note = Notes.findOne(note1._id);
-            console.log(note);
-
-            expect(note.updatedAt).toBeGreaterThan(0);
-
-            expect(note).toInclude({
-                title,
-                body: note1.body
+                expect(Notes.findOne({ _id, userId })).toExist();
             });
 
+            it('not insert note if not authenticated', function () {
+
+                expect(() => {
+                    Meteor.server.method_handlers['notes.insert']();
+                }).toThrow();
+            });
         });
 
-        it('should throw error, additional fields added to update', function () {
-            const title = 'updated title';
-            const other = 'other';
+        describe('remove', function () {
+            it('should remove note', function () {
+                Meteor.server.method_handlers['notes.remove'].apply({ userId: note1.userId }, [note1._id]);
 
-            expect(() => {
+                expect(Notes.findOne({ _id: note1._id })).toNotExist();
+            });
+
+            it('should not remove not, not authenticated', function () {
+
+                expect(() => {
+                    Meteor.server.method_handlers['notes.remove'].apply({}, [note1._id]);
+                }).toThrow();
+            });
+
+            it('should not remove not, no note ID', function () {
+
+                expect(() => {
+                    Meteor.server.method_handlers['notes.remove'].apply({ userId: note1.userId }, ['']);
+                }).toThrow();
+            });
+        });
+
+        describe('update', function () {
+            it('should update note', function () {
+                const title = 'updated title';
                 Meteor.server.method_handlers['notes.update'].apply({
                     userId: note1.userId
                 }, [
                         note1._id,
-                        {
-                            title,
-                            other
-                        }
+                        { title }
                     ]
                 );
-            }).toThrow();
 
+                const note = Notes.findOne(note1._id);
+                console.log(note);
+
+                expect(note.updatedAt).toBeGreaterThan(0);
+
+                expect(note).toInclude({
+                    title,
+                    body: note1.body
+                });
+
+            });
+
+            it('should throw error, additional fields added to update', function () {
+                const title = 'updated title';
+                const other = 'other';
+
+                expect(() => {
+                    Meteor.server.method_handlers['notes.update'].apply({
+                        userId: note1.userId
+                    }, [
+                            note1._id,
+                            {
+                                title,
+                                other
+                            }
+                        ]
+                    );
+                }).toThrow();
+
+            });
+
+            it('should not update note if user was not creator', function () {
+
+                const title = 'updated title';
+                Meteor.server.method_handlers['notes.update'].apply({
+                    userId: 'test Id'
+                }, [
+                        note1._id,
+                        { title }
+                    ]
+                );
+
+                const note = Notes.findOne(note1._id);
+                console.log(note);
+
+                expect(note).toInclude(note1);
+            });
+
+            it('should not update not, not authenticated', function () {
+
+                expect(() => {
+                    Meteor.server.method_handlers['notes.update'].apply({}, [note1._id]);
+                }).toThrow();
+            });
+
+            it('should not update not, no note ID', function () {
+
+                expect(() => {
+                    Meteor.server.method_handlers['notes.update'].apply({ userId: note1.userId }, ['']);
+                }).toThrow();
+            });
         });
 
     });
