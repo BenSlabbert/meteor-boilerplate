ab93b495438d8aee99e9bf356b665a90bba9536e
Ben Slabbert <ben@hex.co.za>
2017-09-17 19:41:36 +0200

    adding new componets to add notes

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import PrivateHeader from './PrivateHeader';
+import NoteList from './NoteList';
 
 // stateless component, presentational
 export default () => {
@@ -8,7 +9,7 @@ export default () => {
         <div>
             <PrivateHeader title="Dashboard" />
             <div className="page-content">
-                Dashboard content.
+                <NoteList />
             </div>
         </div>
     );
