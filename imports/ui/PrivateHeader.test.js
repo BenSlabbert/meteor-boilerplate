1a4669df68ed9e2a57f3d4708fb9dcc42324db4f
Ben Slabbert <ben@hex.co.za>
2017-10-02 13:54:55 +0200

    adding test for NoteListHeader

--- imports/ui/PrivateHeader.test.js
+++ imports/ui/PrivateHeader.test.js
@@ -1,16 +1,17 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import expect from 'expect';
-import { mount } from 'enzyme';
+import {mount} from 'enzyme';
 
-import { PrivateHeader } from './PrivateHeader';
+import {PrivateHeader} from './PrivateHeader';
 
 if (Meteor.isClient) {
 
     describe('PrivateHeader', function () {
 
         it('should set button text to logout', function () {
-            const wrapper = mount(<PrivateHeader title="title" handleLogout={() => { }} />);
+            const wrapper = mount(<PrivateHeader title="title" handleLogout={() => {
+            }}/>);
             const buttonText = wrapper.find('button').text();
             expect(buttonText).toBe('Logout');
         });
@@ -18,14 +19,15 @@ if (Meteor.isClient) {
         it('should use title prop as h1 text', function () {
 
             const title = 'test prop';
-            const wrapper = mount(<PrivateHeader title={title} handleLogout={() => { }} />);
+            const wrapper = mount(<PrivateHeader title={title} handleLogout={() => {
+            }}/>);
             const t = wrapper.find('h1').text();
             expect(t).toBe(title);
         });
 
         it('should call handle logout on click', function () {
             const spy = expect.createSpy();
-            const wrapper = mount(<PrivateHeader title="title" handleLogout={spy} />);
+            const wrapper = mount(<PrivateHeader title="title" handleLogout={spy}/>);
 
             wrapper.find('button').simulate('click');
 
